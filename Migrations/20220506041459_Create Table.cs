617979357ba3534c7bbde7a012262461abd97fb9
m-komilov <muhammadabdulloh.uzinfocom@gmail.com>
2022-05-06 11:45:49 +0500

    Started Crud operation system in ClientApp

---
+++ Migrations/20220506041459_Create Table.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASP.NetCoreWithAngularCrud.Migrations
+{
+    public partial class CreateTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace ASP.NetCoreWithAngularCrud
 {
@@ -23,17 +24,24 @@ namespace ASP.NetCoreWithAngularCrud
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ASP.NetCoreWithAngularCrud", Version = "v1" });
+            });
 
             string connectionString = Configuration.GetConnectionString("default");
             services.AddDbContext<AppDbContext>(c => c.UseSqlServer(connectionString));
 
-            // In production, the Angular files will be served from this directory
-            //services.AddSpaStaticFiles(configuration =>
-            //{
-            //    configuration.RootPath = "ClientApp/dist";
-            //});
+            //view 
 
-            
+            //In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +50,9 @@ namespace ASP.NetCoreWithAngularCrud
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ASP.NetCoreWithAngularCrud v1"));
+
             }
             else
             {
@@ -52,10 +63,13 @@ namespace ASP.NetCoreWithAngularCrud
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            //if (!env.IsDevelopment())
-            //{
-            //    app.UseSpaStaticFiles();
-            //}
+
+            // view
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+            //
 
             app.UseRouting();
 
@@ -66,18 +80,20 @@ namespace ASP.NetCoreWithAngularCrud
                     pattern: "{controller}/{action=Index}/{id?}");
             });
 
-            //app.UseSpa(spa =>
-            //{
-            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            //    // see https://go.microsoft.com/fwlink/?linkid=864501
+            //view 
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
 
-            //    spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "ClientApp";
 
-            //    if (env.IsDevelopment())
-            //    {
-            //        spa.UseAngularCliServer(npmScript: "start");
-            //    }
-            //});
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+            //
         }
     }
 }
